02e45b5b7b65a74a6a4f26a0bd8c731d161e9427
Kenneth Moreland <morelandkd@ornl.gov>
2023-08-29 13:46:46 -0500

    Turn on one-sentance-per-line mode with word wrap mode
    One is often used with the other, and turning on
    one-sentance-per-line mode does not hurt because it just adds a
    key binding if you need it as well as some always helpful
    highlighting.

--- features/textfun.el
+++ features/textfun.el
@@ -28,6 +28,8 @@ spaces) is longer than `fill-column', then the line is left justified."
     )
   )
 
+(require 'one-sentence-per-line)
+(require 'adaptive-wrap)
 (defun toggle-word-wrap-mode ()
   "Toggles whether word wrap is performed with auto-fill-mode, which inserts
 actual line breaks in the document, or with visual-line-mode, which visually
@@ -36,10 +38,15 @@ breaks lines around words but has no actual newline in the document."
   (if visual-line-mode
       (progn (message "Wrap lines with actual newline characters.")
 	     (auto-fill-mode 1)
-	     (visual-line-mode 0))
+	     (visual-line-mode 0)
+             (one-sentence-per-line-mode 0)
+             (adaptive-wrap-prefix-mode 0)
+             )
     (message "Display wrapped lines that have no newlines.")
     (auto-fill-mode 0)
     (visual-line-mode 1)
+    (one-sentence-per-line-mode 1)
+    (adaptive-wrap-prefix-mode 1)
     )
   )
 
